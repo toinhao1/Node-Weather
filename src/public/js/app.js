a771b23940147350b37bbd4ae5a37782fe73b61e
toinhao1 <toinhao89@gmail.com>
2019-04-17 14:22:06 +0200

    Ready to deploy

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,13 +5,14 @@ const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
+
   const location = search.value;
+
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
-  fetch('http://localhost:3000/weather?address=' + location).then(response => {
+  fetch('/weather?address=' + location).then(response => {
     response.json().then(data => {
-      // console.log(data);
       if (data.error) {
         messageOne.textContent = data.error;
       } else {
